9f0cd7abf5f449d4498b5e6e6c9019fb4c5db30e
denys12 <samtallak@gmail.com>
2024-11-07 12:19:52 +0100

    new changes permitted

---
+++ myapp/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
